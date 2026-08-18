6057e0f7d4e88c38a06ba1a5d048f34fe82fd27e
nazar <zyzya>
2026-08-18 13:10:57 +0300

    доработки

--- helper.py
+++ helper.py
@@ -1,6 +1,13 @@
-# ща сделаю здесь explain
-import time
+# ща сделаю здесь explain, ⬇️ import-ы
 import os
+import time
+
+def shutwin(): #выключение винды
+    os.system("shutdown /s /f /t 0")
+
+
+def shuttux(): #выключение линукса
+    os.system("sudo shutdown -h now")
 
 # леха если ты это читаешь есть 3 варианта
 # 1. я скинул тебе это в .py файле
@@ -19,12 +26,17 @@ if voidlnx == "нет": # 1.1
     systemd = input() # 2.1
     if systemd == "да":
         print("зря ты так агент редхат...")
-        os.system("reboot")
+        print("у тебя винда?")
+        winda = input()
+        if winda == "да":
+            shutwin()
+        elif winda == "нет":
+            shuttux()
 
     elif systemd == "нет": # 2.2
         print("молодец, но войд тебе стоит попробывать... ")
         time.sleep(3)
-        os.system("reboot")
+        shuttux()
 
 elif voidlnx == "да": # 1.2
     while True:
@@ -32,7 +44,12 @@ elif voidlnx == "да": # 1.2
         time.sleep(0.1)
 
 elif voidlnx == "сыр.": # 1.3
-    while True:
-        print("СЫРРРР СЫРРР СЫРРРРР СЫР СЫР СЫР СЫР СЫР СЫР СЫР СЫР ")
+        print("СЫРРРР СЫРРР СЫРРРРР СЫР СЫР СЫР СЫР СЫР СЫР СЫР СЫР " * 500)
+        print("ладно ладно")
+        print("тут есть ратник в коде я тебе клянусь")
+        
 
-# туду: сделать что то с сыром 
+        
+
+# туду: сделать что то с сыром ☑️
+
